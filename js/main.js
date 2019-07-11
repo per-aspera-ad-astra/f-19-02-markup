cb80d64d3ca70db40113f541b46976f1b84bc8eb
julia-me <onatskaya.julia@gmail.com>
2019-07-11 14:55:28 +0300

    all files for first module project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+  jcf.replaceAll();
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+      $('.back-to-top').fadeIn(200);    // Fade in the arrow
+  } else {
+      $('.back-to-top').fadeOut(200);   // Else fade out the arrow
+  }
+});
+$('.back-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
