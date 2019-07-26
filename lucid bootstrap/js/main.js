0e8082fe1fc0278e5f2de215b88bcfafb1e7f461
Julia Onatskaya <juliaonatskaya@MacBook-Julia.local>
2019-07-26 14:30:21 +0300

    new file LUCID with Bootstrap

---
+++ lucid bootstrap/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    $('.burger-btn').on('click', function() {
+        $(this).toggleClass('open');
+        $('.menu').toggleClass('active');
+    });
+
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 800,
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }]
+      });
+})
